70047af154dc493a10f7ffabd6a4db55a64c99eb
David Flatman <flatman@email.com>
2020-09-08 12:36:30 -0700

    updated qt/demo.cpp

--- qt/demo.cpp
+++ qt/demo.cpp
@@ -1,22 +1,49 @@
+//------------------------------------------------------------------------------
+///                                                                             
+///@file demo.cpp                                                               
+///                                                                             
+///@brief   Demonstrate that the Qt build is working along with linking         
+///         against the datr.open.source code.                                  
+///                                                                             
+///@version 2020-09-08  DHF     File creation.                                  
+///                                                                             
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+//  Pull in any of the datr.open.source code files.                             
+//------------------------------------------------------------------------------
 #include "lib_bits_work.h"
 
 #include <QApplication>
 #include <QLabel>
+#include <QString>
 
 int main(int argc, char* argv[])
 {
+    //--------------------------------------------------------------------------
+    //  Initialize the Qt application.                                          
+    //--------------------------------------------------------------------------
     QApplication app(argc, argv);
 
-    QLabel  label("Hello World!");
+    //--------------------------------------------------------------------------
+    //  Call some datr.open.source code.                                        
+    //--------------------------------------------------------------------------
+    char memory[] { 0x55, 0xAA};
+
+    QString message("Bit pattern: ");
+
+    for (int i=0; i < sizeof(memory) * 8; ++i)
+    {
+        message += (lib::bits::work::isBitSet(&memory[0], i)) ? "1" : "0";
+    }
+
+
+    //--------------------------------------------------------------------------
+    //  Create a Qt widget, display it, and execute the Qt application.         
+    //--------------------------------------------------------------------------
+    QLabel  label(message);
 
     label.show();
-
-    char memory[] { 0x01, 0xFF, 0x02 };
-
-    if (lib::bits::work::isBitSet(&memory[0], 2))
-    {
-        std::cout << "set" << std::endl;
-    }
 
     return app.exec();
 }
