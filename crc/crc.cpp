3418c79db9bc395505af07d1a1174b094bec2e6a
phongdn <phong.d.nguyen@wsu.edu>
2020-10-20 14:35:47 -0700

    Initial files

---
+++ crc/crc.cpp
@@ -0,0 +1,13 @@
+//------------------------------------------------------------------------------
+///                                                                             
+///@file demo.cpp                                                               
+///                                                                             
+///@brief   Demonstrate that the Qt build is working along with linking         
+///         against the datr.open.source code.                                  
+///                                                                             
+///@version 2020-09-08  DHF     File creation.                                  
+///                                                                             
+//------------------------------------------------------------------------------
+
+#include "crc.h"
+
---
+++ crc/crc.h
@@ -0,0 +1,26 @@
+#ifndef CRC_H_FILE_GUARD
+#define CRC_H_FILE_GUARD
+
+//------------------------------------------------------------------------------
+///@par  Classification:  UNCLASSIFIED, OPEN SOURCE                                                                                                                                                                  
+///@macro THROW                                                                 
+///                                                                             
+///@brief Handle trailing "throw" syntax on routine signatures.                 
+///                                                                             
+///@par Why?                                                                    
+///         Because the geniuses at Microsoft haven't implemented the "throw"   
+///         portion of a method signature and when deriving from a virtual      
+///         method that does properly identify the possible exceptions thrown,  
+///         you have to follow suit.                                            
+///                                                                             
+///                                                                             
+///@version 2013-10-03  DHF     File creation.                                  
+///                                                                             
+//------------------------------------------------------------------------------
+
+class Crc_Checker {
+    public:
+    private:
+};
+
+#endif // #ifndef CRC_H_FILE_GUARD
